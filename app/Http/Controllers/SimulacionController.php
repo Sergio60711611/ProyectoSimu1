db9f050766bad98abd7d520afcce673bc723b059
vivi2901 <vivianespindola29@gmail.com>
2023-11-22 08:49:41 -0400

    Bd

---
+++ app/Http/Controllers/SimulacionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\simulacion;
+use Illuminate\Http\Request;
+
+class SimulacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\simulacion  $simulacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(simulacion $simulacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\simulacion  $simulacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(simulacion $simulacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\simulacion  $simulacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, simulacion $simulacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\simulacion  $simulacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(simulacion $simulacion)
+    {
+        //
+    }
+}
